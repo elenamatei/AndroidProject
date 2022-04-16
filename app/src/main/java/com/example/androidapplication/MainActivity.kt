2e26c9db2a9686a075a51255b210e5b4e38b0296
elenamatei <73391460+elenamatei@users.noreply.github.com>
2022-04-16 15:30:24 +0300

    fixed lateinit bug+ highlight bugs to fix

--- app/src/main/java/com/example/androidapplication/MainActivity.kt
+++ app/src/main/java/com/example/androidapplication/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.androidapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
@@ -19,7 +21,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout : DrawerLayout = findViewById(R.id.drawerLayout)
+        //shareSheet
+        var openShareSheet = findViewById<Button>(R.id.shareSheetButton)
+        openShareSheet.setOnClickListener{
+            shareMethod()
+        }
+
+        //navigationDrawer
+        drawerLayout = findViewById(R.id.drawerLayout)
         val navView : NavigationView = findViewById(R.id.nav_view)
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
         drawerLayout.addDrawerListener(toggle)
@@ -32,7 +41,7 @@ class MainActivity : AppCompatActivity() {
 
             when(it.itemId){
                 R.id.nav_home -> replaceFragment(HomeFragment(), it.title.toString())
-                R.id.nav_login ->  replaceFragment(HomeFragment(), it.title.toString())
+                R.id.nav_login ->  replaceFragment(ProfileFragment(), it.title.toString())
                 R.id.nav_message -> replaceFragment(MessagesFragment(), it.title.toString())
                 R.id.nav_review -> replaceFragment(ReviewFragment(), it.title.toString())
                 R.id.nav_share -> Toast.makeText(applicationContext, "Clicked Share", Toast.LENGTH_SHORT).show()
@@ -64,4 +73,16 @@ class MainActivity : AppCompatActivity() {
 
         return super.onOptionsItemSelected(item)
     }
+
+    fun shareMethod(){
+        var message = "Hi! \n\n"+
+                "This is my sharesheet\n"+
+                "Do you like it? :)"
+        var intent = Intent()
+        intent.type = "text/plain"
+        intent.action = Intent.ACTION_SEND
+        intent.putExtra(Intent.EXTRA_TEXT,message)
+        startActivity(intent)
+    }
+
 }
